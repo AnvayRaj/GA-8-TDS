98d5a3df915a45e90131d27d816a971a4d9521ed
AnvayRaj <78645037+AnvayRaj@users.noreply.github.com>
2023-04-19 19:50:45 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,4 +9,5 @@ a = st.number_input("First Number")
 b = st.number_input("Second Number")
 c = st.number_input("Third Number")
 
-st.write(a,b,c)
+st.write("Greatest Number among the three is :")
+st.write(max(a,b,c))
